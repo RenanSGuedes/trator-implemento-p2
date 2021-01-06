ff7e82739844c6b03d3daaa80a2a745ffe0cd7ce
RenanSGuedes <renanguedes.br@gmail.com>
2021-01-06 20:59:00 -0300

    :cat:

--- efficiency.py
+++ efficiency.py
@@ -3,61 +3,101 @@ from math import exp, sqrt
 '''
 /**
 * todo Eficiência de tração (TE)
-* * MR = Resistência ao rolamento
-* * NT = Força no eixo (pneu): É proveniente da interação solo-pneu entre as
-* * forças GT e MR
-* | NT = Força líquida = GT - MR
-* * Força no implemento -> P = Pv (vertical) + Ph (horizontal)
-* ? Para que haja o movimento
-* | NT >= Ph 
+  * * MR = Resistência ao rolamento
+  * * NT = Força no eixo (pneu): É proveniente da interação solo-pneu entre as
+  * * forças GT e MR
+  * | NT = Força líquida = GT - MR
+  * * Força no implemento -> P = Pv (vertical) + Ph (horizontal)
+  * ? Para que haja o movimento
+  * | NT >= Ph 
 
-* * GT = Tração total (eixo)
-* * MR = Força de resistência ao movimento
-* * wd = Carga dinâmica no pneu
-* * TRR = Taxa de redução da viagem
-* * MRR = Coeficiente de resistência ao rolamento
-* * Índice de mobilidade
-* ? Cálculo do TE
-* | TE (ratio) = OutputPower/InputPower = (NT * va)/AxlePower
-* |              = (NT * va)/(GTR * vt) =
-* |              = ((NT/wd) * va)/((GT/wd) * vt) = 
-* |              = (NTR/GTR) * (va/vt)
+  * * GT = Tração total (eixo)
+  * * MR = Força de resistência ao movimento
+  * * wd = Carga dinâmica no pneu
+  * * TRR = Taxa de redução da viagem
+  * * MRR = Coeficiente de resistência ao rolamento
+  * * Índice de mobilidade
+  * ? Cálculo do TE
+  * | TE (ratio) = OutputPower/InputPower = (NT * va)/AxlePower
+  * |              = (NT * va)/(GTR * vt) =
+  * |              = ((NT/wd) * va)/((GT/wd) * vt) = 
+  * |              = (NTR/GTR) * (va/vt)
 
-* ? Cálculo do GTR - Coeficiente de tração
-* | GTR = (NTR/TE) * (va/vt) = (NRT/TE) * (1 - TRR)
-* * Outra forma de obter o GTR (Equação empírica)
-* | GTR = Q/(r * wd) = 0.88 * (1 - exp(-0.1 * Bn)) * (1 - exp(-7.5 * S)) + 0.04
-* * Bn = Índice de mobilidade - Curva vermelha
-* ? Para cada Bn há uma eficiência TE associada.
-* todo Visando aumentar a eficiência, a melhor alternativa é modificar
-* todo a carga dinâmica (wd), logo
-* | wd = wr (cte) + wl (wl, peso, lastro - pode ser modificado)
-* * S = Patinagem
-* | Bn = (CI * b * d)/wd * (1 + 5 * (add/h)) / (1 + 3 * (b/d))
-* * add = Deformação
-* * h = Altura do rodado
-* * b = Largura do pneu
-* * Diâmetro do pneu
-* * CI = Índice de cone (característica do solo) -> Resistência do solo à penetração
-* * de um cone.
-* | MRR = MR/wd = GTR - NTR = (1/Bn) + 0.4 + (0.5 * S)/sqrt(Bn)
+  * ? Cálculo do GTR - Coeficiente de tração
+  * | GTR = (NTR/TE) * (va/vt) = (NRT/TE) * (1 - TRR)
+  * * Outra forma de obter o GTR (Equação empírica)
+  * | GTR = Q/(r * wd) = 0.88 * (1 - exp(-0.1 * Bn)) * (1 - exp(-7.5 * S)) + 0.04
+  * * Bn = Índice de mobilidade - Curva vermelha
+  * ? Para cada Bn há uma eficiência TE associada.
+  * todo Visando aumentar a eficiência, a melhor alternativa é modificar
+  * todo a carga dinâmica (wd), logo
+  * | wd = wr (cte) + wl (wl, peso, lastro - pode ser modificado)
+  * * S = Patinagem
+  * | Bn = (CI * b * d)/wd * (1 + 5 * (add/h)) / (1 + 3 * (b/d))
+  * * add = Deformação
+  * * h = Altura do rodado
+  * * b = Largura do pneu
+  * * Diâmetro do pneu
+  * * CI = Índice de cone (característica do solo) -> Resistência do solo à penetração
+  * * de um cone.
+  * | MRR = MR/wd = GTR - NTR = (1/Bn) + 0.4 + (0.5 * S)/sqrt(Bn)
 */
 '''
 
-add = float(input('Deformação do pneu: '))
-b = float(input('Largura do pneu sem deformação: '))
-h = float(input('Altura do pneu, medido a partir do topo da roda (sem deformação): '))
-d = float(input('Diâmetro do pneu - a partir do solo - com deformação: '))
-hDeform = float(input('Altura do pneu na parte deformada: '))
+add = float(input('Deflexão/Deformação do pneu (add, em m): '))
+b = float(input('Largura do pneu sem deformação (b, em m): '))
+h = float(input('Altura do pneu, medido a partir do topo da roda (sem deformação) (h, em m): '))
+d = float(input('Diâmetro do pneu - a partir do solo - com deformação [subtrair] (d, em m): '))
 ci = float(input('Índice de cone: '))
-wd = float(input('Carga dinâmica: '))
+wd = float(input('Carga dinâmica: (wd, em N): ')) # * wd = vr
+S = float(input('Patinagem (de 0 a 1): '))
 
-add = h - hDeform # * h será sempre maior que hDeform, já que o pneu
-# * é comprimido na parte em contato com o solo.
 
-Bn = (ci * b * d)/wd * (1 + 5 * (add/h)) / (1 + 3 * (b/d))
+Bn = (ci * b * d)/(wd/1000) * (1 + 5 * (add/h)) / (1 + 3 * (b/d))
 
-S = float(input("Patinagem: "))
-r = float(input("Raio da roda traseira: "))
 
-gtr = Q/(r * wd) = 0.88 * (1 - exp(-0.1 * Bn)) * (1 - exp(-7.5 * S)) + 0.04
+
+print("Bn = {:.2f}".format(Bn))
+
+mrr = 1/Bn + .04 + .5 * S/sqrt(Bn)
+
+mr = mrr * (wd/1000)
+
+print("(Força de resistência ao rolamento) MRR = {:.2f}\n(Coeficiente de resistência ao rolamento) MR = {:.2f} kN".format(mrr, mr))
+
+# * gtr = Q/(r * wd) = 0.88 * (1 - exp(-0.1 * Bn)) * (1 - exp(-7.5 * S)) + 0.04
+
+coefTracao = float(input('Coeficiente de tração: (de 0 a 1): '))
+
+coefTracao = gtr
+
+gt = gtr * (wd/1000)
+
+nt = gt - mr
+
+print("(Tração total/Força de tração máxima) [eixo] GT = {:.2f}".format(gt))
+print("(Força no eixo/máxima força disponível) [pneu] NT = {:.2f}".format(nt))
+
+'''
+  /**
+  * ? Fi = Tipo de solo (1, 2, 3)
+  * ? S = Velocidade (S, em km/h)
+  * ? W = Largura da máquina ou número de ferramentas (em, m)
+  * ? T = Profundidade (T, em cm)
+  */
+'''
+
+f = float(input("Textura do solo (inteiro): "))
+a = float(input("Parâmetro A: "))
+b = float(input("Parâmetro B: "))
+c = float(input("Parâmetro C: "))
+largura = float(input("Largura do implemento (em m)/Número de ferramentas: "))
+profundidade = float(input("Profundidade de trabalho (t, em cm): "))
+velocidade = float(input("Velocidade de operação (km/h): "))
+
+D = f * (a + b * velocidade + c * velocidade**(2)) * largura * profundidade
+
+kwb = D * velocidade
+
+print("D = {:.2f} kN".format(D))
+print("KWB = {:.2f} kN".format(kwb))
--- vh_p.py
+++ vh_p.py
@@ -12,47 +12,46 @@ from math import sin, cos, tan, atan, radians
 
 question = input("Calcular a carga [P] ou a força de tração [V]f: ")
 
-if question == 'Vf':
-  w = float(input("Massa do trator (w, em kg): "))
-  x = float(input("Distância entre eixos medida no plano (x, em m): "))
-  xl = float(input("Distância horizontal que vai do eixo traseiro a CG do implemento (xl, em m): "))
-  yl = float(input("Distância vertical do solo ao eixo inclinado (yl, em m): "))
-  r = float(input("Raio da roda traseira (r, em m): "))
+w = float(input("Massa do trator (w, em kg): "))
+x = float(input("Distância entre eixos medida no plano (x, em m): "))
+xl = float(input("Distância horizontal que vai do eixo traseiro a CG do implemento (xl, em m): "))
+yl = float(input("Distância vertical do solo ao ponto de conexão do implemento (yl, em m): "))
+r = float(input("Raio da roda traseira (r, em m): "))
+yg = float(input("Distãncia vertical do eixo traseiro ao CG do trator (yg, em m): "))
+xr = float(input("Distância horizontal entre o eixo traseiro e o CG do trator (xr, em m): "))
+theta = float(input("Ângulo de inclinação da força p (theta, em graus): "))
+alpha = float(input("Ângulo de inclinação do terreno (alpha, em graus): "))
+
+wr = float(input("Massa com eixo traseiro apoiado (wr, em kg): "))
+wf = float(input("Massa com eixo dianteiro apoiado: (wf, em kg): "))
+
+y = r - yl
+
+if question == 'V':
   p = float(input("Força de tração no implemento (p, em N): "))
-  yg = float(input("Distãncia vertical do solo ao CG do trator (yg, em m): "))
-  xr = float(input("Distância horizontal entre o eixo traseiro e o CG do trator (xr, em m): "))
-  theta = float(input("Ângulo de inclinação da força p (theta, em graus): "))
-  alpha = float(input("Ângulo de inclinação do terreno (alpha, em graus): "))
 
-  y = r - yl
-
-  t1 = w * 9.81 * cos(radians(alpha)) * (xr/x)
-  t2 = w * 9.81 * sin(radians(alpha)) * (r + yg/x)
+  # * t1 = w * 9.81 * cos(radians(alpha)) * (xr/x) # * wf
+  t2 = w * 9.81 * sin(radians(alpha)) * (r + yg)/x
   t3 = p * cos(radians(theta)) * (r - y)/x
   t4 = p * sin(radians(theta)) * xl/x
 
-  vf = t1 - t2 - t3 - t4  
+  # * t5 = w * 9.81 * cos(radians(alpha)) * (1 - xr/x) # * wr
+  t7 = p * cos(radians(theta)) * (yl/x) 
+  t8 = p * sin(radians(theta)) * (x + xl)/x
 
-  print("{:.2f}\n{:.2f}\n{:.2f}\n{:.2f}\n".format(t1, t2, t3, t4))
-  print("vf = {:.2f}N".format(vf))
+
+  vf = wf * 9.81 - t2 - t3 - t4  
+  vr = wr * 9.81 + t2 + t7 + t8
+
+  print("vf = wf (novo) = {:.2f} N = {:.2f} kg".format(vf, vf/9.81))
+  print("vr = {:.2f} N = {:.2f} kg".format(vr, vr/9.81))
+  print("Comparar o vf obtido com o wf do trator no plano para ver o quanto é preciso adicionar ou tirar de massa.")
 else:
-  w = float(input("Massa do trator (w, em kg): "))
   vf = float(input("Tração requerida com o solo (vf, em N): "))
-  x = float(input("Distância entre eixos medida no plano (x, em m): "))
-  xl = float(input("Distância horizontal que vai do eixo traseiro a CG do implemento (xl, em m): "))
-  yl = float(input("Distância vertical do solo ao eixo inclinado (yl, em m): "))
-  r = float(input("Raio da roda traseira (r, em m): "))
-  p = float(input("Força de tração no implemento (p, em N): "))
-  yg = float(input("Distãncia vertical do solo ao CG do trator (yg, em m): "))
-  xr = float(input("Distância horizontal entre o eixo traseiro e o CG do trator (xr, em m): "))
-  theta = float(input("Ângulo de inclinação da força p (theta, em graus): "))
-  alpha = float(input("Ângulo de inclinação do terreno (alpha, em graus): "))
 
-  y = r - yl
+  A = 9.81 * w * (cos(radians(alpha)) * xr - sin(radians(alpha)) * (r + yg)) - vf * x
+  B = cos(radians(theta)) * yl + sin(radians(theta)) * xl
 
-  K = cos(radians(theta)) * (r - y)/x + sin(radians(theta)) * xl/x
-  L = cos(radians(alpha)) * (xr/x) - sin(radians(alpha)) * (r + yg/x)
+  p = A/B
 
-  p = (w * 9.81 * L - vf)/K
-
-  print("p = {}N = {}kg".format(p, p/9.81))
+  print("p = {:.2f} N = {:.2f} kg".format(p, p/9.81))
